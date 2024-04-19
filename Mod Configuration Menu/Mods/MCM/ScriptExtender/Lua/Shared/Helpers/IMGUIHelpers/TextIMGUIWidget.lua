982f15f667ce3735976072d59a24e1310fa41a85
Atílio <27153776+AtilioA@users.noreply.github.com>
2024-04-19 18:59:02 -0300

    fix: change text widget to input text widget in `TextIMGUIWidget`

--- Mod Configuration Menu/Mods/MCM/ScriptExtender/Lua/Shared/Helpers/IMGUIHelpers/TextIMGUIWidget.lua
+++ Mod Configuration Menu/Mods/MCM/ScriptExtender/Lua/Shared/Helpers/IMGUIHelpers/TextIMGUIWidget.lua
@@ -4,7 +4,7 @@ TextIMGUIWidget = _Class:Create("TextIMGUIWidget", IMGUIWidget)
 ---@param value string
 ---@return any
 function TextIMGUIWidget:CreateWidget(group, setting, settingValue, modGUID)
-    local text = group:AddText(setting.Name, settingValue)
+    local text = group:AddInputText(setting.Name, settingValue)
     text.OnChange = function(value)
         BG3MCM:SetConfigValue(setting.Id, value.Value, modGUID)
     end
