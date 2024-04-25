4bc735204a66acd66c58e8a084b1ec5e076e5812
Atílio <27153776+AtilioA@users.noreply.github.com>
2024-04-25 20:11:23 -0300

    refactor: update `folderName` and add (unused) debug option to `Config`

--- Mod Configuration Menu/Mods/MCM/ScriptExtender/Lua/Shared/Helpers/Config.lua
+++ Mod Configuration Menu/Mods/MCM/ScriptExtender/Lua/Shared/Helpers/Config.lua
@@ -1,14 +1,15 @@
 Config = VCConfig:New({
-    folderName = "BG3MCM",
+    folderName = Ext.Mod.GetMod(ModuleUUID).Info.Directory,
     configFilePath = "mod_configuration_menu_config.json",
     defaultConfig = {
         GENERAL = {
             enabled = true, -- Toggle the mod on/off
         },
-        FEATURES = {        -- Options that can override values set by mod authors?
+        FEATURES = {        -- Maybe add options that can override values set by mod authors?
         },
         DEBUG = {
-            level = 0 -- 0 = no debug, 1 = minimal, 2 = verbose debug logs
+            level = 0,           -- 0 = no debug, 1 = minimal, 2 = verbose debug logs
+            write_to_file = true -- Write debug logs to a file (UNIMPLEMENTED)
         },
         onConfigReloaded = {}
     }
