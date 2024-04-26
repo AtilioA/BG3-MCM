aebc2f65063aaf9ad1e8f78294b76610d1f8f4dc
Atílio <27153776+AtilioA@users.noreply.github.com>
2024-04-25 22:21:17 -0300

    refactor: fix enum widget options and initial selection (still kinda broken)

--- Mod Configuration Menu/Mods/MCM/ScriptExtender/Lua/Client/IMGUIHelpers/EnumIMGUIWidget.lua
+++ Mod Configuration Menu/Mods/MCM/ScriptExtender/Lua/Client/IMGUIHelpers/EnumIMGUIWidget.lua
@@ -1,14 +1,22 @@
----@class EnumIMGUIWidget: IMGUIWidget
+---@class EnumIMGUIWidget
 EnumIMGUIWidget = _Class:Create("EnumIMGUIWidget", IMGUIWidget)
 
----@param value any
----@return any
 function EnumIMGUIWidget:CreateWidget(group, setting, settingValue, modGUID)
-    -- REVIEW: is this unpacking necessary?
-    local options = { table.unpack(setting.Options.Choices) }
-    local comboInput = group:AddCombo(setting.Name, settingValue, options)
-    comboInput.OnChange = function(value)
-        IMGUILayer:SetConfigValue(setting.Id, options[value], modGUID)
+    local options = setting.Options.Choices
+    local comboInput = group:AddCombo(setting.Name, settingValue)
+    comboInput.Options = options
+
+    -- Set initial selection
+    for i, value in ipairs(options) do
+        if value == settingValue then
+            comboInput.SelectedIndex = i - 1
+            break
+        end
     end
+
+    comboInput.OnChange = function(value)
+        IMGUILayer:SetConfigValue(setting.Id, options[value.SelectedIndex + 1], modGUID)
+    end
+
     return comboInput
 end
