63ff1816eecefb03b8854d867ed84594b92cfcef
Atílio <27153776+AtilioA@users.noreply.github.com>
2024-04-25 21:06:06 -0300

    refactor: (temporarily) use `AddSliderInt` instead of `AddSlider`

--- Mod Configuration Menu/Mods/MCM/ScriptExtender/Lua/Client/IMGUIHelpers/SliderIMGUIWidget.lua
+++ Mod Configuration Menu/Mods/MCM/ScriptExtender/Lua/Client/IMGUIHelpers/SliderIMGUIWidget.lua
@@ -4,7 +4,7 @@ SliderIMGUIWidget = _Class:Create("SliderIMGUIWidget", IMGUIWidget)
 ---@param value number
 ---@return any
 function SliderIMGUIWidget:CreateWidget(group, setting, settingValue, modGUID)
-    local slider = group:AddSlider(setting.Name, settingValue, setting.Options.Min, setting.Options.Max)
+    local slider = group:AddSliderInt(setting.Name, settingValue, setting.Options.Min, setting.Options.Max)
     slider.OnChange = function(value)
         IMGUILayer:SetConfigValue(setting.Id, value.Value[1], modGUID)
     end
