c9d3a29e40d1edc0937f1fb6c8c993bf79ccda4e
Atílio <27153776+AtilioA@users.noreply.github.com>
2024-04-27 22:18:20 -0300

    refactor: rename MCMIMGUIAPI to IMGUIAPI

--- Mod Configuration Menu/Mods/MCM/ScriptExtender/Lua/Client/_Init.lua
+++ Mod Configuration Menu/Mods/MCM/ScriptExtender/Lua/Client/_Init.lua
@@ -1,14 +1,5 @@
----Ext.Require files at the path
----@param path string
----@param files string[]
-function RequireFiles(path, files)
-    for _, file in pairs(files) do
-        Ext.Require(string.format("%s%s.lua", path, file))
-    end
-end
-
 RequireFiles("Client/", {
     "IMGUILayer",
     "IMGUIHelpers/_Init",
-    "MCMIMGUIAPI",
+    "IMGUIAPI",
 })
