794262695df743b3450079f38f3260810c795c53
Atílio <27153776+AtilioA@users.noreply.github.com>
2024-05-01 23:14:50 -0300

    fix: return early on `SliderIntValidator` (as to not compare ranges with NaN)

--- Mod Configuration Menu/Mods/BG3MCM/ScriptExtender/Lua/Shared/Helpers/Validators/SliderIntValidator.lua
+++ Mod Configuration Menu/Mods/BG3MCM/ScriptExtender/Lua/Shared/Helpers/Validators/SliderIntValidator.lua
@@ -3,6 +3,10 @@ SliderIntValidator = _Class:Create("SliderIntValidator", Validator)
 
 function SliderIntValidator.Validate(config, value)
     local isValueNumber = type(value) == "number"
+    if not isValueNumber then
+        return false
+    end
+
     local isValueWithinRange = value >= config.Options.Min and value <= config.Options.Max
-    return isValueNumber and isValueWithinRange
+    return isValueWithinRange
 end
