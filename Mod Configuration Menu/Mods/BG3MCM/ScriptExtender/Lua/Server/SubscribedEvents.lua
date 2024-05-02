52f783700e7c677f1cfaf2172850f1af31de8e44
Atílio <27153776+AtilioA@users.noreply.github.com>
2024-05-01 23:15:20 -0300

    refactor: remove unused MCM config option from `SubscribedEvents`

--- Mod Configuration Menu/Mods/BG3MCM/ScriptExtender/Lua/Server/SubscribedEvents.lua
+++ Mod Configuration Menu/Mods/BG3MCM/ScriptExtender/Lua/Server/SubscribedEvents.lua
@@ -1,25 +1,23 @@
 SubscribedEvents = {}
 
 function SubscribedEvents.SubscribeToEvents()
-    if Config:getCfg().GENERAL.enabled == true then
-        -- When resetting Lua states
-        -- Ext.Events.ResetCompleted:Subscribe(EHandlers.OnReset)
+    -- When resetting Lua states
+    -- Ext.Events.ResetCompleted:Subscribe(EHandlers.OnReset)
 
-        Ext.Osiris.RegisterListener("LevelGameplayStarted", 2, "before", EHandlers.OnLevelGameplayStarted)
-        --- Message handler for when the (IMGUI) client requests the MCM settings to be loaded
-        Ext.RegisterNetListener(Channels.MCM_CLIENT_REQUEST_CONFIGS, EHandlers.OnClientRequestConfigs)
-        Ext.RegisterConsoleCommand('mcm_reset', function() MCM:LoadAndSendSettings() end)
+    Ext.Osiris.RegisterListener("LevelGameplayStarted", 2, "before", EHandlers.OnLevelGameplayStarted)
+    --- Message handler for when the (IMGUI) client requests the MCM settings to be loaded
+    Ext.RegisterNetListener(Channels.MCM_CLIENT_REQUEST_CONFIGS, EHandlers.OnClientRequestConfigs)
+    Ext.RegisterConsoleCommand('mcm_reset', function() MCM:LoadAndSendSettings() end)
 
-        --- Message handler for when the (IMGUI) client requests a setting to be set
-        Ext.RegisterNetListener(Channels.MCM_CLIENT_REQUEST_SET_SETTING_VALUE, EHandlers.OnClientRequestSetSettingValue)
+    --- Message handler for when the (IMGUI) client requests a setting to be set
+    Ext.RegisterNetListener(Channels.MCM_CLIENT_REQUEST_SET_SETTING_VALUE, EHandlers.OnClientRequestSetSettingValue)
 
-        --- Message handler for when the (IMGUI) client requests a setting to be reset
-        Ext.RegisterNetListener(Channels.MCM_CLIENT_REQUEST_RESET_SETTING_VALUE,
-            EHandlers.OnClientRequestResetSettingValue)
+    --- Message handler for when the (IMGUI) client requests a setting to be reset
+    Ext.RegisterNetListener(Channels.MCM_CLIENT_REQUEST_RESET_SETTING_VALUE,
+        EHandlers.OnClientRequestResetSettingValue)
 
-        --- Message handler for when the (IMGUI) client requests a profile to be set
-        Ext.RegisterNetListener(Channels.MCM_CLIENT_REQUEST_SET_PROFILE, EHandlers.OnClientRequestSetProfile)
-    end
+    --- Message handler for when the (IMGUI) client requests a profile to be set
+    Ext.RegisterNetListener(Channels.MCM_CLIENT_REQUEST_SET_PROFILE, EHandlers.OnClientRequestSetProfile)
 end
 
 return SubscribedEvents
--- Mod Configuration Menu/Mods/BG3MCM/ScriptExtender/Lua/Shared/Helpers/Config.lua
+++ Mod Configuration Menu/Mods/BG3MCM/ScriptExtender/Lua/Shared/Helpers/Config.lua
@@ -4,15 +4,8 @@ Config = VCConfig:New({
     folderName = Ext.Mod.GetMod(ModuleUUID).Info.Directory,
     configFilePath = "mod_configuration_menu_config.json",
     defaultConfig = {
-        GENERAL = {
-            enabled = true, -- Toggle the mod on/off
-        },
-        FEATURES = {
-            open_on_start = true, -- Show the MCM window when the game starts
-        },
         DEBUG = {
             level = 0,           -- 0 = no debug, 1 = minimal, 2 = verbose debug logs
-            write_to_file = true -- Write debug logs to a file (UNIMPLEMENTED)
         },
         onConfigReloaded = {}
     }
