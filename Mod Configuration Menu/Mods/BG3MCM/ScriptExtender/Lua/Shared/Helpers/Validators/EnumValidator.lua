e11300b75097728eea7923f01077db14e20d2728
Atílio <27153776+AtilioA@users.noreply.github.com>
2024-05-08 00:43:23 -0300

    fix: add safeguards to enum validation

--- Mod Configuration Menu/Mods/BG3MCM/ScriptExtender/Lua/Shared/Helpers/Validators/EnumValidator.lua
+++ Mod Configuration Menu/Mods/BG3MCM/ScriptExtender/Lua/Shared/Helpers/Validators/EnumValidator.lua
@@ -2,6 +2,11 @@
 EnumValidator = _Class:Create("EnumValidator", Validator)
 
 function EnumValidator.Validate(config, value)
+    local hasOptions = config.Options ~= nil and config.Options.Choices ~= nil
+    if not hasOptions then
+        return false
+    end
+
     local isValueInOptions = table.contains(config.Options.Choices, value)
     return isValueInOptions
 end
