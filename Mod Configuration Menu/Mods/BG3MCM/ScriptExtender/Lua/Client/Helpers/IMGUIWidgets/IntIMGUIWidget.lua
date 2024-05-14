aef2197db55cc046efc7cb3d1a7c36f86d6113ab
Atílio <27153776+AtilioA@users.noreply.github.com>
2024-05-13 21:58:24 -0300

    feat: add increment buttons to `int` and `slider_int` for finetuning

--- Mod Configuration Menu/Mods/BG3MCM/ScriptExtender/Lua/Client/Helpers/IMGUIWidgets/IntIMGUIWidget.lua
+++ Mod Configuration Menu/Mods/BG3MCM/ScriptExtender/Lua/Client/Helpers/IMGUIWidgets/IntIMGUIWidget.lua
@@ -3,10 +3,33 @@ IntIMGUIWidget = _Class:Create("IntIMGUIWidget", IMGUIWidget)
 
 function IntIMGUIWidget:new(group, setting, initialValue, modGUID)
     local instance = setmetatable({}, { __index = IntIMGUIWidget })
+
+    -- 'Previous' button
+    instance.PreviousButton = group:AddButton("-")
+    instance.PreviousButton.IDContext = "PreviousButton_" .. setting.Id
+    instance.PreviousButton.OnClick = function()
+        local newValue = instance.Widget.Value[1] - 1
+        instance:UpdateCurrentValue(newValue)
+        IMGUIAPI:SetSettingValue(setting.Id, newValue, modGUID)
+    end
+
+    -- Actual int input widget
     instance.Widget = group:AddInputInt("", initialValue)
     instance.Widget.OnChange = function(value)
         IMGUIAPI:SetSettingValue(setting.Id, value.Value[1], modGUID)
     end
+    instance.Widget.SameLine = true
+
+    -- 'Next' button
+    instance.NextButton = group:AddButton("+")
+    instance.NextButton.IDContext = "NextButton_" .. setting.Id
+    instance.NextButton.OnClick = function()
+        local newValue = instance.Widget.Value[1] + 1
+        instance:UpdateCurrentValue(newValue)
+        IMGUIAPI:SetSettingValue(setting.Id, newValue, modGUID)
+    end
+    instance.NextButton.SameLine = true
+
     return instance
 end
 
--- Mod Configuration Menu/Mods/BG3MCM/ScriptExtender/Lua/Client/Helpers/IMGUIWidgets/SliderIntIMGUIWidget.lua
+++ Mod Configuration Menu/Mods/BG3MCM/ScriptExtender/Lua/Client/Helpers/IMGUIWidgets/SliderIntIMGUIWidget.lua
@@ -4,10 +4,33 @@ SliderIntIMGUIWidget = _Class:Create("SliderIntIMGUIWidget", IMGUIWidget)
 ---@return any
 function SliderIntIMGUIWidget:new(group, setting, initialValue, modGUID)
     local instance = setmetatable({}, { __index = SliderIntIMGUIWidget })
+
+    -- 'Previous' button
+    instance.PreviousButton = group:AddButton("<")
+    instance.PreviousButton.IDContext = "PreviousButton_" .. setting.Id
+    instance.PreviousButton.OnClick = function()
+        local newValue = math.max(setting.Options.Min, instance.Widget.Value[1] - 1)
+        instance:UpdateCurrentValue(newValue)
+        IMGUIAPI:SetSettingValue(setting.Id, newValue, modGUID)
+    end
+
+    -- Actual slider
     instance.Widget = group:AddSliderInt("", initialValue, setting.Options.Min, setting.Options.Max)
     instance.Widget.OnChange = function(value)
         IMGUIAPI:SetSettingValue(setting.Id, value.Value[1], modGUID)
     end
+    instance.Widget.SameLine = true
+
+    -- 'Next' button
+    instance.NextButton = group:AddButton(">")
+    instance.NextButton.IDContext = "NextButton_" .. setting.Id
+    instance.NextButton.OnClick = function()
+        local newValue = math.min(setting.Options.Max, instance.Widget.Value[1] + 1)
+        instance:UpdateCurrentValue(newValue)
+        IMGUIAPI:SetSettingValue(setting.Id, newValue, modGUID)
+    end
+    instance.NextButton.SameLine = true
+
     return instance
 end
 
