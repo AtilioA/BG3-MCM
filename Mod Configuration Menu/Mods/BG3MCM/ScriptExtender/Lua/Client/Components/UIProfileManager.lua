e0c1216dc4aa0e96a3462cc359b51337e202b8d7
Atílio <27153776+AtilioA@users.noreply.github.com>
2024-05-26 19:06:07 -0300

    Reapply "refactor: use localization for profile management"
    This reverts commit a5abf2da0af90c73ac521a1874569b9f62aed92c.

--- Mod Configuration Menu/Mods/BG3MCM/ScriptExtender/Lua/Client/Components/UIProfileManager.lua
+++ Mod Configuration Menu/Mods/BG3MCM/ScriptExtender/Lua/Client/Components/UIProfileManager.lua
@@ -14,29 +14,33 @@ end
 
 local function getDeleteProfileButtonLabel(profile)
     if profile == "Default" then
-        return "Cannot delete the default profile."
+        return Ext.Loca.GetTranslatedString("hfdf59b69495c4aeca03f38977a00a69d431c")
     else
-        return "Delete profile '" .. profile .. "'"
+        return VCString:UpdateLocalizedMessage("h75b86690333d4937a1737fe8daddde41ga10", profile)
     end
 end
 
 --- Create widgets for managing profiles (selecting, creating, deleting)
 function UIProfileManager:CreateProfileCollapsingHeader()
-
     local profiles = MCMAPI:GetProfiles()
     local currentProfile = MCMAPI:GetCurrentProfile()
     local profileIndex = UIProfileManager:FindProfileIndex(currentProfile) - 1
 
-    local profileCollapsingHeader = MCM_WINDOW:AddCollapsingHeader("Profile management")
-    local profileCombo = profileCollapsingHeader:AddCombo("Select profile")
+    local profileCollapsingHeader = MCM_WINDOW:AddCollapsingHeader(Ext.Loca.GetTranslatedString(
+    "hb7ee77283bd94bd5b9d3fe696b45e85ae804"))
+    profileCollapsingHeader:AddSeparatorText(Ext.Loca.GetTranslatedString("h2082b6b6954741ef970486be3bb77ad53782"))
+    local profileCombo = profileCollapsingHeader:AddCombo("")
 
     profileCombo.Options = profiles.Profiles
     profileCombo.SelectedIndex = profileIndex or 1
 
+    -- Create/delete profile buttons
     profileCollapsingHeader:AddSeparator()
-    profileCollapsingHeader:AddText("Create a new profile")
+
+    profileCollapsingHeader:AddSeparatorText(Ext.Loca.GetTranslatedString("h5788159872f84825b184d42c1fbd6a216541"))
     local newProfileName = profileCollapsingHeader:AddInputText("")
-    local profileButton = profileCollapsingHeader:AddButton("Create")
+    local profileButton = profileCollapsingHeader:AddButton(Ext.Loca.GetTranslatedString(
+    "h3e4b68e2569e4df2b548b4a5a893a57a7972"))
     profileButton.SameLine = true
 
     local deleteProfileButton = profileCollapsingHeader:AddButton(getDeleteProfileButtonLabel(MCMAPI:GetCurrentProfile()))
@@ -65,6 +69,7 @@ function UIProfileManager:UpdateDeleteProfileButton(deleteProfileButton, profile
 end
 
 function UIProfileManager:SetupDeleteProfileButton(deleteProfileButton, profileCombo)
+    deleteProfileButton.IDContext = "MCM_deleteProfileButton"
     deleteProfileButton.OnClick = function()
         local currentProfile = MCMAPI:GetCurrentProfile()
         if currentProfile ~= "Default" then
@@ -86,6 +91,7 @@ end
 
 function UIProfileManager:SetupCreateProfileButton(profileButton, newProfileName, profileCombo,
                                                    deleteProfileButton)
+    profileButton.IDContext = "MCM_createProfileButton"
     profileButton.OnClick = function()
         if newProfileName.Text ~= "" then
             MCMAPI:CreateProfile(newProfileName.Text)
@@ -105,6 +111,7 @@ function UIProfileManager:SetupCreateProfileButton(profileButton, newProfileName
 end
 
 function UIProfileManager:SetupProfileComboOnChange(profileCombo, deleteProfileButton)
+    profileCombo.IDContext = "MCM_profileCombo"
     profileCombo.OnChange = function(inputChange)
         local selectedIndex = inputChange.SelectedIndex + 1
         local selectedProfile = inputChange.Options[selectedIndex]
