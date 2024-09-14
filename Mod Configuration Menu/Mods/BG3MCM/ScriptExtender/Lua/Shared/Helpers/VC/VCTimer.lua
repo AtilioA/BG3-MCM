f35d46fb5db37a9f699efaadb914750ecf48f70d
Atílio <27153776+AtilioA@users.noreply.github.com>
2024-09-13 21:07:17 -0300

    chore: improve VCTimer variable names

--- Mod Configuration Menu/Mods/BG3MCM/ScriptExtender/Lua/Shared/Helpers/VC/VCTimer.lua
+++ Mod Configuration Menu/Mods/BG3MCM/ScriptExtender/Lua/Shared/Helpers/VC/VCTimer.lua
@@ -68,19 +68,19 @@ function VCTimer:OnTicksAndTime(ticks, time, fn, ticksOrTime)
     end
 end
 
---- Calls the callback with an interval and stops calling the callback when the totalTime is reached.
+--- Calls the callback with an intervalInMs and stops calling the callback when the totalTimeInMs is reached.
 --- @param callback function The callback to call.
---- @param interval integer The interval to wait before calling the callback again.
---- @param totalTime integer The total time to call the callback.
-function VCTimer:CallWithInterval(callback, interval, totalTime)
-    if totalTime <= 0 then
+--- @param intervalInMs integer The interval to wait before calling the callback again.
+--- @param totalTimeInMs integer The total time to call the callback.
+function VCTimer:CallWithInterval(callback, intervalInMs, totalTimeInMs)
+    if totalTimeInMs <= 0 then
         return
     end
 
     local elapsedTime = 0
 
     local function invokeCallback()
-        if elapsedTime >= totalTime then
+        if elapsedTime >= totalTimeInMs then
             return
         end
 
@@ -89,14 +89,14 @@ function VCTimer:CallWithInterval(callback, interval, totalTime)
             return
         end
 
-        elapsedTime = elapsedTime + interval
-        if elapsedTime < totalTime then
-            Ext.Timer.WaitFor(interval, invokeCallback)
+        elapsedTime = elapsedTime + intervalInMs
+        if elapsedTime < totalTimeInMs then
+            Ext.Timer.WaitFor(intervalInMs, invokeCallback)
         end
     end
 
-    if interval > totalTime then
-        interval = totalTime
+    if intervalInMs > totalTimeInMs then
+        intervalInMs = totalTimeInMs
     end
 
     invokeCallback()
@@ -104,7 +104,7 @@ end
 
 --- Repeatedly calls the main callback at specified intervals until the condition callback returns true.
 --- @param mainCallback function The primary function to execute.
---- @param intervalMs integer The time interval in milliseconds between each call of the main callback.
+--- @param intervalMs integer The time intervalInMs in milliseconds between each call of the main callback.
 --- @param conditionCallback function A function that returns true to stop further execution of the main callback.
 function VCTimer:ExecuteWithIntervalUntilCondition(mainCallback, intervalMs, conditionCallback)
     local function attemptCallbackExecution()
