c54b96cc44f108b12fbf94073c1050b2a293e28f
Atílio <27153776+AtilioA@users.noreply.github.com>
2025-04-17 16:09:03 -0300

    fix(Validators): introduce `FloatUtils` for epsilon comparison

---
+++ Mod Configuration Menu/Mods/BG3MCM/ScriptExtender/Lua/Shared/Helpers/Validators/FloatUtils.lua
@@ -0,0 +1,21 @@
+---@class FloatUtils
+local FloatUtils = {}
+
+-- TODO: make this configurable in a setting-by-setting basis, since some settings may be more sensitive than others
+-- This should be a good start for now
+FloatUtils.EPSILON = 1e-6
+
+--- Checks if a value is within [min, max] with epsilon tolerance.
+--- If min or max is nil, that bound is ignored.
+function FloatUtils.isWithinEpsilon(value, min, max, epsilon)
+    epsilon = epsilon or FloatUtils.EPSILON
+    if min ~= nil and value < min - epsilon then
+        return false
+    end
+    if max ~= nil and value > max + epsilon then
+        return false
+    end
+    return true
+end
+
+return FloatUtils
