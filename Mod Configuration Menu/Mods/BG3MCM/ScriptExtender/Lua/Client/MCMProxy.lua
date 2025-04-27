e0aa36267932bb0ab1d4480e64483f0585467c10
Atílio <27153776+AtilioA@users.noreply.github.com>
2025-04-26 22:50:11 -0300

    refactor(MCMProxy): streamline game state management and improve tab insertion logic

--- Mod Configuration Menu/Mods/BG3MCM/ScriptExtender/Lua/Client/MCMProxy.lua
+++ Mod Configuration Menu/Mods/BG3MCM/ScriptExtender/Lua/Client/MCMProxy.lua
@@ -1,43 +1,11 @@
 --- MCMProxy ensures that mod settings can be managed and updated from the main menu if necessary, or from the server if the game is running.
 ---
 ---@class MCMProxy
----@field GameState string The current game state. Might be used to determine if the game is in the main menu.
----@field GameStateSubject any Subject that emits game state changes
-MCMProxy = _Class:Create("MCMProxy", nil, {
-    GameState = Ext.Net.IsHost() and "Running" or "Menu",
-    GameStateSubject = nil
-})
-
-local RX = Ext.Require("Lib/reactivex/_init.lua")
-
--- Initialize the reactive game state management
-function MCMProxy:Initialize()
-    self.GameStateSubject = RX.BehaviorSubject.Create(self.GameState)
-
-    Ext.Events.GameStateChanged:Subscribe(function(e)
-        MCMProxy.GameState = e.ToState
-
-        -- Emit the state change through our reactive subject
-        if self.GameStateSubject then
-            self.GameStateSubject:OnNext(MCMProxy.GameState)
-        end
-
-        MCMDebug(1, "GameState changed to " .. tostring(MCMProxy.GameState))
-    end)
-end
+MCMProxy = _Class:Create("MCMProxy", nil, {})
 
 function MCMProxy.IsMainMenu()
-    local gameState = MCMProxy.GameState
-
-    if not gameState and Ext.Net.IsHost then
-        return not Ext.Net.IsHost()
-    end
-
-    if gameState == 'Menu' then
-        return true
-    end
-
-    return false
+    local gameState = Ext.Utils.GetGameState()
+    return gameState == 'Menu'
 end
 
 function MCMProxy:LoadConfigs()
@@ -56,23 +24,13 @@ function MCMProxy:LoadConfigs()
 end
 
 function MCMProxy:InsertModMenuTab(modUUID, tabName, tabCallback)
-    if not self.GameStateSubject then
-        self:Initialize()
-    end
-
-    -- Subscribe to game state changes to handle tab insertion appropriately
+    -- Handle initial state
     local disclaimerTab, disclaimerElement
-    local subscription = nil
-    subscription = self.GameStateSubject:Subscribe(function(gameState)
-        -- This timer is a workaround. Ideally, we should be able to use this value directly. May refactor this if we get a way to query the game state directly.
+    local function handleGameState(gameState)
         VCTimer:OnTicks(2, function()
             if gameState == "Menu" then
-                -- We're in the main menu
                 MCMClientState.UIReady:Subscribe(function(ready)
-                    if not ready then
-                        return
-                    end
-
+                    if not ready then return end
                     -- Add temporary message to inform users that custom MCM tabs are not available in the main menu
                     if disclaimerTab or disclaimerElement then return end
                     disclaimerTab, disclaimerElement = DualPane:CreateTabWithDisclaimer(
@@ -83,15 +41,19 @@ function MCMProxy:InsertModMenuTab(modUUID, tabName, tabCallback)
                 if disclaimerElement then
                     xpcall(function() disclaimerElement.Label = "" end, function() end)
                 end
-
                 MCMClientState.UIReady:Subscribe(function(ready)
-                    if ready and subscription and not subscription._unsubscribed then
+                    if ready then
                         DualPane:InsertModTab(modUUID, tabName, tabCallback)
-                        subscription = nil
                     end
                 end)
             end
         end)
+    end
+
+    handleGameState(Ext.Utils.GetGameState())
+
+    Ext.Events.GameStateChanged:Subscribe(function(e)
+        handleGameState(e.ToState)
     end)
 end
 
@@ -107,9 +69,7 @@ function MCMProxy:SetSettingValue(settingId, value, modUUID, setUIValue)
     if MCMProxy.IsMainMenu() then
         -- Handle locally
         MCMAPI:SetSettingValue(settingId, value, modUUID)
-        if setUIValue then
-            setUIValue(value)
-        end
+        if setUIValue then setUIValue(value) end
     else
         -- Send to server
         Ext.Net.PostMessageToServer(NetChannels.MCM_CLIENT_REQUEST_SET_SETTING_VALUE, Ext.Json.Stringify({
@@ -119,13 +79,8 @@ function MCMProxy:SetSettingValue(settingId, value, modUUID, setUIValue)
         }))
         -- Check if server updated the setting
         ModEventManager:Subscribe(EventChannels.MCM_SETTING_SAVED, function(data)
-            if data.modUUID ~= modUUID or data.settingId ~= settingId then
-                return
-            end
-
-            if setUIValue then
-                setUIValue(data.value)
-            end
+            if data.modUUID ~= modUUID or data.settingId ~= settingId then return end
+            if setUIValue then setUIValue(data.value) end
         end)
     end
 end
@@ -153,6 +108,3 @@ function MCMProxy:RegisterMCMKeybindings()
     InputCallbackManager.RegisterKeybinding(ModuleUUID, "toggle_mcm_sidebar_keybinding",
         function() IMGUIAPI:ToggleMCMSidebar() end)
 end
-
--- Initialize the proxy when the module is loaded
-MCMProxy:Initialize()
