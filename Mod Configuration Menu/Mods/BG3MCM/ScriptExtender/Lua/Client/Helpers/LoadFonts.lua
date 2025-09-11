a04915ce338e738132acd9e482cea05e98814285
Atílio <27153776+AtilioA@users.noreply.github.com>
2025-09-11 11:22:54 -0300

    chore: move fonts location

--- Mod Configuration Menu/Mods/BG3MCM/ScriptExtender/Lua/Client/Helpers/LoadFonts.lua
+++ Mod Configuration Menu/Mods/BG3MCM/ScriptExtender/Lua/Client/Helpers/LoadFonts.lua
@@ -1,4 +1,4 @@
-local fontPath = "Mods/BG3MCM/GUI/Assets/Fonts/"
+local fontPath = "Mods/BG3MCM/Assets/Fonts/"
 
 for k, s in pairs(Font.FONT_SIZE_OPTIONS) do
     Ext.IMGUI.LoadFont("OpenDyslexic" .. k, fontPath .. "OpenDyslexic/OpenDyslexic.ttf", s)
