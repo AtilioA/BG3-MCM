9ecd2a3302caf80ed7d8ab4fbdcc793fb1c4926e
Atílio <27153776+AtilioA@users.noreply.github.com>
2025-09-11 12:11:47 -0300

    fix: display checkbox tooltip on label hover as well

--- Mod Configuration Menu/Mods/BG3MCM/ScriptExtender/Lua/Client/Components/IMGUIWidgets/CheckboxIMGUIWidget.lua
+++ Mod Configuration Menu/Mods/BG3MCM/ScriptExtender/Lua/Client/Components/IMGUIWidgets/CheckboxIMGUIWidget.lua
@@ -5,6 +5,11 @@ function CheckboxIMGUIWidget:new(group, setting, initialValue, modUUID)
     -- Ensure Widget is an instance-specific property. This is some Lua nonsense that I don't understand. Try designing a better language next time. /hj
     local instance = setmetatable({}, { __index = CheckboxIMGUIWidget })
 
+    instance.UserData = {
+        ModUUID = modUUID,
+        LabelWidget = nil
+    }
+
     instance.Widget = group:AddCheckbox("", initialValue)
     instance.Widget.OnChange = function(value)
         IMGUIAPI:SetSettingValue(setting.Id, value.Checked, modUUID)
@@ -13,7 +18,7 @@ function CheckboxIMGUIWidget:new(group, setting, initialValue, modUUID)
 
     local opts = setting:GetOptions() or {}
     if opts["InlineTitle"] ~= false then
-        instance:_addTitleText(group, setting, modUUID)
+        instance:_addLabelWidget(group, setting, modUUID)
     end
 
     return instance
@@ -23,26 +28,28 @@ end
 ---@param group any
 ---@param setting BlueprintSetting
 ---@param modUUID string
-function CheckboxIMGUIWidget:_addTitleText(group, setting, modUUID)
-    local titleText = setting:GetLocaName() or setting:GetId()
-    if titleText == "" then return end
+function CheckboxIMGUIWidget:_addLabelWidget(group, setting, modUUID)
+    local LabelWidget = setting:GetLocaName() or setting:GetId()
+    if LabelWidget == "" then return end
 
-    local checkboxTitleText = group:AddText(titleText)
+    local checkboxLabelWidget = group:AddText(LabelWidget)
 
-    checkboxTitleText.OnClick = function()
+    checkboxLabelWidget.OnClick = function()
         IMGUIAPI:SetSettingValue(setting.Id, not self.Widget.Checked, modUUID)
     end
 
-    checkboxTitleText.OnHoverEnter = function()
+    checkboxLabelWidget.OnHoverEnter = function()
         self.Widget:SetColor("FrameBg", UIStyle.UnofficialColors.BoxHoverColor)
     end
 
-    checkboxTitleText.OnHoverLeave = function()
+    checkboxLabelWidget.OnHoverLeave = function()
         self.Widget:SetColor("FrameBg", UIStyle.UnofficialColors.BoxColor)
     end
 
-    checkboxTitleText.TextWrapPos = 0
-    checkboxTitleText.SameLine = true
+    checkboxLabelWidget.TextWrapPos = 0
+    checkboxLabelWidget.SameLine = true
+
+    self.UserData.LabelWidget = checkboxLabelWidget
 end
 
 function CheckboxIMGUIWidget:UpdateCurrentValue(value)
@@ -51,4 +58,9 @@ end
 
 function CheckboxIMGUIWidget:GetOnChangeValue(value)
     return value.Checked
+end
+
+function CheckboxIMGUIWidget:SetupTooltip(widget, setting)
+    IMGUIWidget:SetupTooltip(self.UserData.LabelWidget, setting)
+    return IMGUIWidget:SetupTooltip(widget, setting)
 end
