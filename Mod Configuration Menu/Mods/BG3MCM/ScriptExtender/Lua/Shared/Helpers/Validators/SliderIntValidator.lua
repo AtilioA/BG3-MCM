fa38623a0380a7c2225acc13ea564b9a7561e939
Atílio <27153776+AtilioA@users.noreply.github.com>
2025-10-18 15:58:19 -0300

    fix: validate slider_int are integers

--- Mod Configuration Menu/Mods/BG3MCM/ScriptExtender/Lua/Shared/Helpers/Validators/SliderIntValidator.lua
+++ Mod Configuration Menu/Mods/BG3MCM/ScriptExtender/Lua/Shared/Helpers/Validators/SliderIntValidator.lua
@@ -1,9 +1,15 @@
 ---@class SliderIntValidator: Validator
 SliderIntValidator = _Class:Create("SliderIntValidator", Validator)
 
+--- Better validation could truncate the value to an integer
 function SliderIntValidator.Validate(config, value)
     local isValueNumber = type(value) == "number"
     if not isValueNumber then
+        return false
+    end
+
+    local isValueInteger = math.floor(value) == value
+    if not isValueInteger then
         return false
     end
 
